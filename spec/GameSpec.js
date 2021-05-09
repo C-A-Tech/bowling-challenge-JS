696bb4c7e3e17a6fea02d1e00140a4c303baedb6
Cynthia Anyaeriuba <cynthia.a@Cynthias-MacBook-Pro.local>
2021-05-09 23:50:42 +0100

    game always starts with a score of 0

---
+++ spec/GameSpec.js
@@ -0,0 +1,21 @@
+describe("Game", function() {
+  var game;
+
+  beforeEach(function() {
+    game = new Game();
+  });
+
+  it("always starts with a score of 0", function() {
+    expect(game.score).toEqual(0);
+  })
+
+
+
+
+
+
+
+
+
+
+});
